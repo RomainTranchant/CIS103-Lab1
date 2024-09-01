358a7a44ff3127a1b679f6572f26d0d64d5399b6
RomainTranchant <romaintranchant@me.com>
2024-09-01 18:06:59 -0500

    Add files via upload

---
+++ lab0.py
@@ -0,0 +1,22 @@
+# Lab 1: Getting Started with Python
+# CIS 103: Introduction to Programming
+# Instructor: MD Ali
+# Student Name: Romain Tranchant
+# Date: 08/26/2024
+
+# This script is a simple Python program that prints "Hello, welcome to CIS 103"
+
+# The print() function is used to display output to the console.
+# The following line of code will print the string "Hello, welcome to CIS 103"
+
+def main():
+    # The following line of code will print the string "Hello welcome to CIS 103"
+    print("Hello, welcome to CIS 103!")
+    # The print() function is used to display output to the console
+
+if __name__ == "__main__":
+    main()
+# This if __name__ == "__main__": block is used to ensure that the main()
+# function is called only when this script is executed directly, and not
+# when it is imported as a module.
+# This line calls the main() function to execute the code within
